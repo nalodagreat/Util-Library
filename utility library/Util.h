e0c55b31d48fa876fff7e3ce62ab40677d4587d8
nalodagreat <nalothegreat0@gmail.com>
2025-08-05 13:37:02 +0200

    generate random number

--- utility library/Util.h
+++ utility library/Util.h
@@ -5,5 +5,18 @@
 using namespace std;
 class Util
 {
-    
+    public:
+    static void  Srand()
+    {
+        //Seeds the random number generator in C++, called only once
+        srand((unsigned)time(NULL));
+    }
+
+    static  int randomNumber(int From, int To)
+    {
+        //Function to generate a random number
+        int randNum = rand() % (To - From + 1) + From;
+        return randNum;
+    }
+
 };
